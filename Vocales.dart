09079586eb60ab5d497ab589f83438ff7a51cbaf
Andres Buitrago <andresbuitrago274062@correo.itm.edu.co>
2023-10-24 16:56:33 -0500

    nuevo

--- Vocales.dart
+++ Vocales.dart
@@ -1,3 +1,12 @@
+import 'dart:io';
+
+void main() {
+  print("Ingrese una palabra: ");
+  String palabra = stdin.readLineSync()!;
+  int cantidadVocales = contarVocales(palabra);
+  print("Cantidad de vocales en la palabra \"$palabra\": $cantidadVocales");
+}
+
 int contarVocales(String palabra) {
   int contador = 0;
   for (int i = 0; i < palabra.length; i++) {
